6bbe732ac96306039442b6ee177724a3887959fd
Nigel Tufnel <tenorideout@gmail.com>
2018-06-26 19:55:55 -0400

    closer to being finished - reset working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,8 +10,8 @@ var questionlist;
 var index = 0;
 
 function reset() {
-   
-    questionlist;
+
+    // questionlist;
     index = 0;
     $("#question").empty();
     $(".choice1").empty();
@@ -20,8 +20,9 @@ function reset() {
     $(".choice4").empty();
     correct = 0;
     wrong = 0;
-    $(this).attr("disabled", false)
-    myButton.disabled = false; return false
+    // if (!onsubmit) { return; }
+    $("#startGame").attr("disabled", false)
+    // myButton.enabled = false; return false
 }
 
 // Function to populate the DOM with the next question - in Array order
@@ -33,22 +34,26 @@ function newQuestion() {
         $(".choice2").empty();
         $(".choice3").empty();
         $(".choice4").empty();
+        $("#enter").empty()
         $("#question").append(
             `<div>${questionList[index].question}</div>`
 
         )
         $(".choice1").append(
-            `${"<input type='radio' value='0' name='firstQ'> " + questionList[index].answers[0]}`
+            `<input type='radio' value='0' name=question${index}> ${questionList[index].answers[0]}`
         )
         $(".choice2").append(
-            `${"<input type='radio' value='1' name='firstQ'> " + questionList[index].answers[1]}`
+            `<input type='radio' value='1' name=question${index}> ${questionList[index].answers[1]}`
         )
         $(".choice3").append(
-            `${"<input type='radio' value='2' name='firstQ'> " + questionList[index].answers[2]}`
+            `<input type='radio' value='2' name=question${index}> ${questionList[index].answers[2]}`
         )
         $(".choice4").append(
-            `${"<input type='radio' value='3' name='firstQ'> " + questionList[index].answers[3]}`
+            `<input type='radio' value='3' name=question${index}> ${questionList[index].answers[3]}`
         )
+        $("#enter").append(`
+        <button type="submit" value="submit" disabled id="submitButton" class="btn btn-secondary button${index}">ENTER</button>
+        `)
     }
 }
 
@@ -65,7 +70,8 @@ function gameOver() {
 // function to start game by clicking on the startGame button //
 
 $("#startGame").on("click", function () {
-    setTimeout(gameOver, 1000 * 10);
+    // setTimeout(gameOver, 1000 * 10);
+    // if(!onsubmit) {return;}
     $(this).attr("disabled", true)
     // timer will be set to 45 seconds - but for now it's 2 for testing//
     // $("#game-over").css("display", "none");
@@ -78,23 +84,30 @@ $("#startGame").on("click", function () {
 
 // Function to take the click function after user has made choice - that will calculate 
 // correct and wrong choices - or end of the game after all questions have been asnwered. 
+// if($(`input[name=question${index}]:checked`).val() === undefined){
 
-$("#submitButton").on("click", function () {
-    var answer = $("input[name=firstQ]:checked").val()
-    console.log(typeof answer);
+// }
+// $(`input[name=question${index}]`).change(function () { $("#submitButton").prop("disabled", false); });
+$(document).on("change", `input:radio`, function () {
+    $(`.button${index}`).attr("disabled", false)
+})
+$(document).on("click", "#submitButton", function () {
+    var answer = $(`input[name=question${index}]:checked`).val()
+    console.log(answer);
+    console.log("correct answer", questionList[index].correctAns)
     index++;
 
-    if (index > questionList.length) {
+    if (index > questionList.length - 1) {
         gameOver();
     }
     else if (answer == questionList[index].correctAns) {
-            correct++;
-            newQuestion();
-            console.log(correct);
-        }
+        correct++;
+        newQuestion();
+        // console.log(correct);
+    }
     else {
-            wrong++;
-            newQuestion();
-            console.log(wrong);
-        }
-    })
+        wrong++;
+        newQuestion();
+        // console.log(wrong);
+    }
+})
